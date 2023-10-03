b30a5795c4121af722fd5b0525c86bc87167d593
ming-macos <mengmengxiao@msn.com>
2023-10-03 12:49:34 +0800

    feat: add initial version

---
+++ README.md
@@ -0,0 +1,50 @@
+# CueMode README
+
+This is the README for the Visual Studio Code extension "CueMode". This extension is designed to enhance text readability and focus by providing a unique "Cue Mode" functionality when you are presenting or recording your screen. Just select the text you want to display or just open a text file then click right mouse button and select "Cue Mode" from the context menu. 
+
+## Features
+
+CueMode changes the visual style of your code in a few key ways to enhance readability:
+
+* Increases font size
+* Adjusts line height
+* Centers the layout
+* Changes color scheme for high contrast
+
+This mode is perfect for presentations, educational purposes, or just those moments when you need to cut through the clutter and focus on your content.
+
+You can customize these settings to suit your preferences:
+
+* Color theme (default is "Classic" - white text on black background)
+* Maximum content width in pixels (default is 800)
+* Font size in points (default is 24)
+* Line height in ems (default is 1.5)
+* Padding in pixels (default is 10)
+* Scrolling speed (default is 0.1)
+
+![Demo](./demo.gif)
+
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `cuemode.colorTheme`: Set the color theme for the cue mode.
+* `cuemode.maxWidth`: Set the maximum content width in pixels.
+* `cuemode.fontSize`: Set the font size in points.
+* `cuemode.lineHeight`: Set the line height in ems.
+* `cuemode.padding`: Set the padding in pixels.
+* `cuemode.scrollSpeed`: Set the scrolling speed.
+
+## Known Issues
+
+There are no known issues at this time. 
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of CueMode.
+
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,96 @@
+const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+    console.log('Congratulations, your extension "cuemode" is now active!');
+
+    let disposable = vscode.commands.registerCommand('cuemode.cueMode', function () {
+        const config = vscode.workspace.getConfiguration('cuemode');
+
+        const colorTheme = config.get('colorTheme');
+        const maxWidth = config.get('maxWidth');
+        const fontSize = config.get('fontSize');
+        const lineHeight = config.get('lineHeight');
+        const padding = config.get('padding');
+        const scrollSpeed = config.get('scrollSpeed');
+
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            let document = editor.document;
+            let selection = editor.selection;
+
+            // Get the selected text or the entire document if nothing is selected
+            let text;
+            if (!selection.isEmpty) {
+                text = document.getText(selection);
+            } else {
+                text = document.getText();
+            }
+
+            // Get the filename of the current document
+            let filename = path.basename(document.uri.fsPath);
+
+            const panel = vscode.window.createWebviewPanel(
+                'cueMode',
+                `${filename} - Cue Mode`,
+                vscode.ViewColumn.One,
+                {
+                    // Enable scripts in the webview
+                    enableScripts: true,
+
+                    // And restrict the webview to only loading content from our extension's `media` directory.
+                    localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'media'))],
+
+                    // Make sure all keyboard events are sent to the webview
+                    retainContextWhenHidden: true
+                }
+            );
+
+            // Pass the selected text to the webview
+            panel.webview.html = getWebviewContent(text, colorTheme, maxWidth, fontSize, lineHeight, padding, scrollSpeed);
+        }
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+function getWebviewContent(text, colorTheme, maxWidth, fontSize, lineHeight, padding, scrollSpeed) {
+    const colorThemes = {
+        "classic": { "color": "white", "backgroundColor": "black" },
+        "inverted": { "color": "black", "backgroundColor": "white" },
+        "midnightBlue": { "color": "#282C34", "backgroundColor": "#8C92AC" },
+        "sunset": { "color": "#FF4500", "backgroundColor": "#FFD700" },
+        "forest": { "color": "#228B22", "backgroundColor": "#F5F5DC" },
+        "ocean": { "color": "#1E90FF", "backgroundColor": "#F0F8FF" },
+        "rose": { "color": "#DB7093", "backgroundColor": "#FFF0F5" },
+    };    
+
+    const color = colorThemes[colorTheme].color;
+    const backgroundColor = colorThemes[colorTheme].backgroundColor;
+
+    const htmlPath = path.join(__dirname, 'webview.html');
+    let html = fs.readFileSync(htmlPath, 'utf8');
+
+    html = html.replace('${color}', color);
+    html = html.replace('${backgroundColor}', backgroundColor);
+    html = html.replace('${maxWidth}', maxWidth);
+    html = html.replace('${fontSize}', fontSize);
+    html = html.replace('${lineHeight}', lineHeight);
+    html = html.replace('${padding}', padding);
+    html = html.replace('${scrollSpeed}', scrollSpeed);
+    html = html.replace('${text}', text);
+
+    return html;
+}
+
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+}
